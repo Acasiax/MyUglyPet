f75660fcd887388b18a87dbe464c462c71f813a7
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-08-14 16:38:39 +0900

    Initial Commit

---
+++ MyUglyPet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyUglyPet
+//
+//  Created by 이윤지 on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
