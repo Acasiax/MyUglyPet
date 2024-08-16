1a906b901fe21baf75f5e7f4482b337c9beefb24
Acasiax <117105267+Acasiax@users.noreply.github.com>
2024-08-16 16:42:21 +0900

    사진 추가

--- MyUglyPet/New Group/View/BaseGameView.swift
+++ MyUglyPet/New Group/View/BaseGameView.swift
@@ -13,18 +13,23 @@ class BaseGameView: UIViewController {
     let worldCupLabel: UILabel = {
         let label = UILabel()
         label.text = "망한 사진 월드컵"
-        label.backgroundColor = .orange
-        label.layer.cornerRadius = 10
+        label.backgroundColor = UIColor(red: 1.00, green: 0.53, blue: 0.67, alpha: 1.00)
+        label.textColor = .white // 텍스트 색상을 변경할 수 있습니다.
         label.font = UIFont.boldSystemFont(ofSize: 15)
         label.textAlignment = .center
+        label.layer.cornerRadius = 13 // 높이의 절반으로 설정 (타원형)
+        label.clipsToBounds = true // 레이블의 모서리를 둥글게 잘라냄
+        
         return label
     }()
+
     
     let titleLabel: UILabel = {
         let label = UILabel()
-        label.text = "누가 더 망한 사진인지 골라보세요!"
+        label.text = "둘 중에 \n 더 망한 사진을 골라보세요!"
         label.font = UIFont.boldSystemFont(ofSize: 24)
         label.textAlignment = .center
+        label.numberOfLines = 0
         return label
     }()
     
@@ -89,7 +94,7 @@ class BaseGameView: UIViewController {
     let secondImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
-        imageView.image = UIImage(named: "기본냥")
+        imageView.image = UIImage(named: "기본냥멍2")
         imageView.clipsToBounds = true
         imageView.layer.cornerRadius = 50
         return imageView
@@ -148,7 +153,15 @@ class BaseGameView: UIViewController {
         worldCupLabel.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
             make.centerX.equalToSuperview()
+            make.height.equalTo(40) // 높이를 설정
+            make.width.equalTo(130) // 텍스트보다 넓게 설정
         }
+
+        
+//        worldCupLabel.snp.makeConstraints { make in
+//            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+//            make.centerX.equalToSuperview()
+//        }
 
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(worldCupLabel.snp.bottom).offset(20)
--- MyUglyPet/New Group/View/GameViewController.swift
+++ MyUglyPet/New Group/View/GameViewController.swift
@@ -18,11 +18,12 @@ class GameViewController: BaseGameView {
     
     let pets: [Pet] = [
         Pet(name: "벼루님", age: "냥생 3개월차", image: UIImage(named: "기본냥")!),
-        Pet(name: "꼬질이님", age: "견생 5년차", image: UIImage(named: "기본냥")!),
-        Pet(name: "3님", age: "냥생 3개월차", image: UIImage(named: "기본냥")!),
-        Pet(name: "4님", age: "견생 5년차", image: UIImage(named: "기본냥")!),
-        Pet(name: "5", age: "냥생 3개월차", image: UIImage(named: "기본냥")!),
-        Pet(name: "6님", age: "견생 5년차", image: UIImage(named: "기본냥")!)
+        Pet(name: "꼬질이님", age: "견생 5년차", image: UIImage(named: "기본냥멍2")!),
+        Pet(name: "3님", age: "냥생 3개월차", image: UIImage(named: "기본냥멍3")!),
+        Pet(name: "4님", age: "견생 5년차", image: UIImage(named: "기본냥멍4")!),
+        Pet(name: "5", age: "냥생 3개월차", image: UIImage(named: "기본냥멍5")!),
+        Pet(name: "6님", age: "견생 5년차", image: UIImage(named: "기본냥멍6")!),
+        Pet(name: "7님", age: "견생 5년차", image: UIImage(named: "기본냥멍7")!)
        
     ]
     let rounds: [String] = ["32강", "16강", "8강", "4강"]
@@ -103,22 +104,26 @@ extension GameViewController {
         firstContainerView.transform = CGAffineTransform(translationX: 0, y: view.frame.height)
         secondContainerView.transform = CGAffineTransform(translationX: 0, y: view.frame.height)
         
-        // titleLabel 애니메이션 초기 설정 (살짝 아래에 위치)
+        // 첫 번째 컨테이너 애니메이션
+        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.6, options: .curveEaseInOut, animations: {
+            self.firstContainerView.transform = .identity  // 첫 번째 컨테이너 원래 위치로 복원
+        }, completion: nil)
+        
+        // 두 번째 컨테이너 애니메이션 (첫 번째 컨테이너 시작 후 0.2초 지연)
+        UIView.animate(withDuration: 1.5, delay: 0.08, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.6, options: .curveEaseInOut, animations: {
+            self.secondContainerView.transform = .identity  // 두 번째 컨테이너 원래 위치로 복원
+        }, completion: nil)
+        
+        // titleLabel과 worldCupLabel 애니메이션 (첫 번째 컨테이너와 동시에 시작)
         titleLabel.transform = CGAffineTransform(translationX: 0, y: 10)
         worldCupLabel.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
         
-        // 애니메이션 블록
-        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.6, options: .curveEaseInOut, animations: {
-            self.firstContainerView.transform = .identity  // 원래 위치로 복원
-            self.secondContainerView.transform = .identity
-        }, completion: nil)
-        
-        // titleLabel 애니메이션 (아래에서 위로 살짝 올라오는 효과)
         UIView.animate(withDuration: 1.0, delay: 0.3, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
-            self.titleLabel.transform = .identity // 원래 위치로 복원
-            self.worldCupLabel.transform = .identity // 원래 크기로 복원
+            self.titleLabel.transform = .identity
+            self.worldCupLabel.transform = .identity
         }, completion: nil)
     }
+
     
     // 살짝 확대된 후 복원하는 애니메이션
     func animateContainerView(_ view: UIView) {
